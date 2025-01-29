95fdf35b88ff498f053ba3f935d91a7de8a794c3
tayfyvz <tyfn51@gmail.com>
2025-01-29 18:43:29 -0500

    Single Number solution

---
+++ src/singleNumber.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+        int ans = nums[0];
+
+        for (int i = 1; i < nums.size(); i++)
+        {
+            ans ^= nums[i];
+        }
+
+        return ans;
+    }
+};
