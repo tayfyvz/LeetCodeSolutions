a115c251040d1cbd33c08fedcadeefb3fc292180
tayfyvz <tyfn51@gmail.com>
2025-02-21 16:05:56 -0500

    Symmetric Tree solution

---
+++ src/LeetCodeCS/SymmetricTree.cs
@@ -0,0 +1,41 @@
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class SymmetricTree
+{
+    public bool IsSymmetric(TreeNode root) 
+    {
+        return CheckSymmetric(root.left, root.right);
+    }
+
+    private bool CheckSymmetric(TreeNode p1, TreeNode p2)
+    {
+        if (p1 == null && p2 == null)
+        {
+            return true;
+        }
+        
+        if (p1 == null || p2 == null)
+        {
+            return false;
+        }
+        
+        if (p1.val != p2.val)
+        {
+            return false;
+        }
+        
+        bool cond1 = CheckSymmetric(p1.left, p2.right);
+        bool cond2 = CheckSymmetric(p1.right, p2.left);
+        
+        return cond1 && cond2;
+    }
+}
