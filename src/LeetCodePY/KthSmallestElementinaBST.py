5872f3797e549074363e7015ac70fd6c3261ce4b
tayfyvz <tyfn51@gmail.com>
2025-11-06 22:44:51 -0500

    Create KthSmallestElementinaBST.py

---
+++ src/LeetCodePY/KthSmallestElementinaBST.py
@@ -0,0 +1,26 @@
+from collections import deque
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+
+        self.count = 0
+        self.res = 0
+        def dfs(node):
+
+            if not node:
+                return
+
+            dfs(node.left)
+            self.count += 1
+
+            if self.count == k:
+                self.res = node.val
+                return
+            dfs(node.right)
+        dfs(root)
+        return self.res
