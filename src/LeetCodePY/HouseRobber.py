93ab83b9299544f56b5446d9a1cbaa5f302fea8d
tayfyvz <tyfn51@gmail.com>
2025-11-07 15:35:19 -0500

    Create HouseRobber.py

---
+++ src/LeetCodePY/HouseRobber.py
@@ -0,0 +1,18 @@
+from typing import List
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
+
+        dp = [0] * (len(nums))
+        dp[0] = nums[0]
+        dp[1] = max(nums[1], nums[0])
+
+        for i in range(2, len(dp)):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+
+        return dp[-1]
